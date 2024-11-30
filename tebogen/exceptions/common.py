da1033d9e15a8e054633e1ddc44cebe62cf6f28c
catfield123 <gtagta65@gmail.com>
2024-11-30 23:46:08 +0300

    exceptions for validating string as python variable name

---
+++ tebogen/exceptions/common.py
@@ -0,0 +1,9 @@
+class NotValidPythonVariableNameException(Exception):
+    def __init__(self, message = None, details = None):
+        super().__init__(message)
+        self.details = details
+
+class PythonKeywordException(Exception):
+    def __init__(self, message = None, details = None):
+        super().__init__(message)
+        self.details = details
