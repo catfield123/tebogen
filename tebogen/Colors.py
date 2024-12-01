de216a864bd2747df423c6fa4c84aae81deb2fc0
catfield123 <gtagta65@gmail.com>
2024-12-01 05:26:45 +0300

    add blue color

--- tebogen/Colors.py
+++ tebogen/Colors.py
@@ -6,12 +6,14 @@ class Colors:
     GREEN = curses.COLOR_GREEN
     BLACK = curses.COLOR_BLACK
     YELLOW = curses.COLOR_YELLOW
+    BLUE = curses.COLOR_BLUE
 
     WHITE_BLACK = None
     BLACK_WHITE = None
     RED_BLACK = None
     GREEN_BLACK = None
     YELLOW_BLACK = None
+    BLUE_BLACK = None
 
     @staticmethod
     def init_colors() -> None:
@@ -20,10 +22,12 @@ class Colors:
         curses.init_pair(3, Colors.RED, Colors.BLACK)
         curses.init_pair(4, Colors.GREEN, Colors.BLACK)
         curses.init_pair(5, Colors.YELLOW, Colors.BLACK)
+        curses.init_pair(6, Colors.BLUE, Colors.BLACK)
 
         Colors.WHITE_BLACK = curses.color_pair(1)
         Colors.BLACK_WHITE = curses.color_pair(2)
         Colors.RED_BLACK = curses.color_pair(3)
         Colors.GREEN_BLACK = curses.color_pair(4)
         Colors.YELLOW_BLACK = curses.color_pair(5)
+        Colors.BLUE_BLACK = curses.color_pair(6)
 
